7e4cf334b205260a5166813963bbcdb1e184064a
marco <lazzari.marco.ml@gmail.com>
2024-03-18 15:18:36 +0100

    dati stampano a schermo

--- index.php
+++ index.php
@@ -52,6 +52,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+    <ul>
+        <?php
+            foreach($hotels as $currentHotel){
+                echo "
+                <li>
+                    <ul>";
+                        foreach($currentHotel as $key => $value){
+                            echo "<li>$key: $value";
+                        }
+                echo "
+                    </ul>
+                </li>";
+                // <li>
+                //     <ul>";
+                //     foreach($currentHotel as $key => $value){
+                //         echo"
+                //         <li>
+                //             $key: $value
+                //         <li>";
+                //     }
+                //     echo "
+                //     <ul>
+                // <li>";
+            }
+        ?>
+    </ul>
     
 
     <!-- bootstrap -->
